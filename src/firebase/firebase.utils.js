e8b831250dfda1b985aad36b2aa5c2b97c9482d0
ChironJon <immrwhiskers4@gmail.com>
2020-09-01 02:52:27 -0500

    created user and cart sagas for sign in, sign up and sign in with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
     measurementId: "G-8SWRM2REGL"
   };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
 	if (!userAuth) return;
 
@@ -71,13 +73,20 @@ export const convertCollectionsSnapshotToMap = (collections) => {
 	}, {});
 }
 
-firebase.initializeApp(config);
+export const getCurrentUser = () => {
+	return new Promise((resolve, reject) => {
+		const unsubscribe = auth.onAuthStateChanged(userAuth => {
+			unsubscribe();
+			resolve(userAuth);
+		}, reject)
+	})
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
