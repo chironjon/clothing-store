c7b48f4f9776425e6b84784be95c0d7aa46eea17
ChironJon <immrwhiskers4@gmail.com>
2020-10-12 17:55:40 -0500

    m

--- client/src/components/sign-up/sign-up.component.jsx
+++ client/src/components/sign-up/sign-up.component.jsx
@@ -6,8 +6,6 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import { signUpStart } from '../../redux/user/user.actions';
 
-// import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
-
 import { SignUpContainer, TitleContainer } from './sign-up.styles';
 
 const SignUp = ({ signUpStart }) => {
